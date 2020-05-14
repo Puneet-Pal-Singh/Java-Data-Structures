7851028b83747aae2f2b835368a889fe321c09d3
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-05-14 22:30:25 +0530

    Opt solution for pattern 15

--- 01. Java Basics/02. Patterns/15. Pattern 15/Pattern 15 - Opt Solution.java
+++ 01. Java Basics/02. Patterns/15. Pattern 15/Pattern 15 - Opt Solution.java
@@ -15,28 +15,17 @@ public class Main{
                  
         for(int crows = 1; crows <= nrows; crows++) //
         {  
-             
-            // int val = 0;
-                    
-            //for lower half decrement -- 5th step
-        /*
-            if(crows <= nrows/2){
-                val = crows;
-            }else{
-                val = nrows - crows + 1;
-            }
-        */
-        
             for(int cspace = 1; cspace <= nspace; cspace++)
             {
-                System.out.print("\t");  //"\t -- backSlash t"
+                System.out.print("\t");  
             }
+            
             int val = outval;
+            
             for(int cstars = 1; cstars <= nstars; cstars++)
             {
-                System.out.print(val + "\t"); //"\t -- backSlash t"
+                System.out.print(val + "\t");
                     
-                //for upper half decrement--4th step
                 if(cstars <= nstars/2){
                     val++;
                 }else{
@@ -64,43 +53,52 @@ public class Main{
             
 /*
 
-     -------Step 1-------	
+     -------Step 1-------   
 
-        	*	
-	*	*	*	
-*	*	*	*	*	
-	*	*	*	
-		*     
+        *   
+    *   *   *   
+*   *   *   *   *   
+    *   *   *   
+        *     
 
      -------Step 2-------
-        	1	
-	1	1	1	
-1	1	1	1	1	
-	1	1	1	
-		1
-		
+        1   
+    1   1   1   
+1   1   1   1   1   
+    1   1   1   
+        1
+        
      -------Step 3-------
-        	1	
-	2   	3	4	
-3	4	5	6	7	
-	4	5	6	
-		5
-		
+        1   
+    2   2   2   
+3   3   3   3   3   
+    4   4   4   
+        5
+        
      -------Step 4------
 
-        	1	
-	2	3	2	
-3	4	5	4	3	
-	4	5	4	
-		5
-		
+        1   
+    2   2   2   
+3   3   3   3   3   
+    2   2   2   
+        5
+        
+     -------Step 5------
+
+        1   
+    2   3   4   
+3   4   5   6   7   
+    2   3   4   
+        1
+        
+
      -------Step Final--------
 
-        	1	
-	2	3	2	
-3	4	5	4	3	
-	2	3	2	
-		1     	
+        1   
+    2   3   2   
+3   4   5   4   3   
+    2   3   2   
+        1       
 
 
 */       

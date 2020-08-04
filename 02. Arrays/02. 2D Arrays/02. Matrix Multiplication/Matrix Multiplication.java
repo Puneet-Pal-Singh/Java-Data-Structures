d1c3760c3b784e3e9297d461ef4027bd4706959e
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-04 20:14:41 +0530

    Update Matrix Multiplication.java

--- 02. Arrays/02. 2D Arrays/02. Matrix Multiplication/Matrix Multiplication.java
+++ 02. Arrays/02. 2D Arrays/02. Matrix Multiplication/Matrix Multiplication.java
@@ -1,59 +1,53 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    public static void main(String[] args) throws Exception 
-    {
-       Scanner scr=new Scanner(System.in);
-       int n1=scr.nextInt();
-       int m1=scr.nextInt();
-       int [][] a=new int [n1][m1];
-       for(int i=0;i<n1;i++)  //i<a.length
-       {
-           for(int j=0;j<m1;j++)  //j<a[0].length
-           {
-             a[i][j] =scr.nextInt();  
-           }
-       }
-       int n2=scr.nextInt();
-       int m2=scr.nextInt();
-       int [][] b=new int [n2][m2];
-       for(int i=0;i<n2;i++)  //i<b.length
-       {
-           for(int j=0;j<m2;j++)  //j<b[0].length
-           {
-             b[i][j] =scr.nextInt();  
-           }
-       }
-       
-       if(m1 != n2)
-       {
-           System.out.println("Invalid input");
-           return;
-       }
-        int [][] prd=new int [n1][m2];
-       for(int i=0;i<n1;i++)  
-       {
-           for(int j=0;j<m2;j++)
-           {
-               for(int k=0;k<b.length;k++)
-               {
-                  prd[i][j] +=a[i][k]*b[k][j];  
-               }
-           }
-       }
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int r1 = scn.nextInt();
+        int c1 = scn.nextInt();
+        int[][] arr1 = new int[r1][c1];
+        for (int i = 0; i < arr1.length; i++) //i<r1
+        {
+            for (int j = 0; j < arr1[0].length; j++) //j<c1
+            {
+                arr1[i][j] = scn.nextInt();
+            }
+        }
+        int r2 = scn.nextInt();
+        int c2 = scn.nextInt();
+        int[][] arr2 = new int[r2][c2];
+        for (int i = 0; i < arr2.length; i++) // i < n2
+        {
+            for (int j = 0; j < arr2[0].length; j++) // j < m2
+            {
+                arr2[i][j] = scn.nextInt();
+            }
+        }
+
+        if (c1 != r2) {
+            System.out.println("Invalid input");
+            return;
+        }
         
-        for(int i=0;i<prd.length;i++)  
-       {
-           for(int j=0;j<prd[0].length;j++)
-           {
-             System.out.print(prd[i][j]+" ");   
-           }
-           System.out.println();
-       }
+        int[][] prd = new int[r1][c2];
+        for (int i = 0; i < prd.length; i++) {        // i < r1
+            for (int j = 0; j < prd[0].length; j++) {     // j < c2
+                for (int k = 0; k < arr2.length; k++) {     // k < c1 or r2
+                    prd[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < prd.length; i++) {
+            for (int j = 0; j < prd[0].length; j++) {
+                System.out.print(prd[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-    
+
 }
 
 

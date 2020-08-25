672f02eaa869b9b3d72a2a7e6dc5848208738f5b
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-25 14:49:07 +0530

    Update Power-logarithmic.java

--- 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/06. Power-logarithmic/Power-logarithmic.java
+++ 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/06. Power-logarithmic/Power-logarithmic.java
@@ -7,16 +7,27 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int x = Integer.parseInt(br.readLine());
     int n = Integer.parseInt(br.readLine());
-    int p = power(x, n);
-    System.out.println(p);
+    int xpn = power(x, n);
+    
+    /*
+    
+    // Using Scanner
+    Scanner scn = new Scanner(System.in);
+    int x = scn.nextInt();
+    int n = scn.nextInt();
+    int xpn = power(x, n);
+    
+    */
+    
+    System.out.println(xpn);
   }
 
   public static int power(int x, int n) {
     if(n == 0){
       return 1;
     }
-    int xpnb2 = power(x, n / 2);
-    int xpn = xpnb2 * xpnb2;
+    int xpnb2 = power(x, n / 2);      // x raise to power n/2
+    int xpn = xpnb2 * xpnb2;          // x raise to power n 
 
     if(n % 2 == 1){
       xpn = xpn * x;

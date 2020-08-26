367e1a8c77dce8e05ca9faf41ef127cddfa18c41
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-26 17:10:51 +0530

    Update First Index.java

--- 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/12. First Index/First Index.java
+++ 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/12. First Index/First Index.java
@@ -4,52 +4,58 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Scanner scr= new Scanner(System.in);
-       int n=scr.nextInt();
-       int[] arr=new int[n];;
-       
-       for(int i=0;i<n;i++)
-         arr[i]=scr.nextInt();
-         
-       int x=scr.nextInt();
-       
-       int firstval=firstIndex(arr,0,x);
-       System.out.print(firstval);
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];;
+
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int d = scn.nextInt();
+
+        int fi = firstIndex(arr, 0, d);
+        System.out.print(fi);
     }
 
-    public static int firstIndex(int[] arr, int idx, int x){
-        
-        if(idx == arr.length-1){  return -1;}
-        
-        if( arr[idx] == x )
-        {
-          return idx; 
+    public static int firstIndex(int[] arr, int idx, int x) {
+
+        if (idx == arr.length) {
+            return -1;
         }
         
-        int fo=firstIndex(arr,idx+1,x);
+        /*
+        int fiisa = firstIndex(arr, idx + 1, x);
+        if( arr[idx] == x ){
+            return idx;  
+        } else {
+            return fiisa;
+        }
+        */
+        
+        // Better Approach
+        if (arr[idx] == x) {
+            return idx;
+        } else {
+            int fiisa = firstIndex(arr, idx + 1, x);
+            return fiisa;
+        }
 
-        // if( arr[idx] == x )
-        // {
-        //   return idx;  // fo=idx;
-        // }
-         return fo;
     }
+}    
 
-}
-
-/*
-        Sample Input--
-        
-        6
-        15
-        11
-        40
-        4
-        4
-        9
-        4
-        
-        Sample Output---
-        
-        3
-*/
+    /*
+            Sample Input--
+            
+            6
+            15
+            11
+            40
+            4
+            4
+            9
+            4
+            
+            Sample Output---
+            
+            3
+    */

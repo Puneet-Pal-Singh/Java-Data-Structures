3cef4e1f22b499afab744c867d040f9c57664a74
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-26 23:07:26 +0530

    Update Last Index.java

--- 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/13. Last Index/Last Index.java
+++ 03. Recursion and Backtracking/01. Basics and Control Flow in Recursion/13. Last Index/Last Index.java
@@ -4,23 +4,40 @@ import java.util.*;
 public class Main {
 
    public static void main(String[] args) throws Exception {
+      
+      /*
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int n = Integer.parseInt(br.readLine());
+      
       int[] arr = new int[n];
-
       for (int i = 0; i < n; i++) {
          arr[i] = Integer.parseInt(br.readLine());
       }
+      
       int x = Integer.parseInt(br.readLine());
-      int fi = lastIndex(arr, 0, x);
-      System.out.println(fi);
+      int li = lastIndex(arr, 0, x);
+      System.out.println(li);
+      */
+      
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      
+      int[] arr = new int[n];
+      for (int i = 0; i < arr.length; i++) {
+         arr[i] = scn.nextInt();
+      }
+      
+      int x = scn.nextInt();
+      int li = lastIndex(arr, 0, x);
+      System.out.println(li);
+      
    }
 
    public static int lastIndex(int[] arr, int idx, int x) {
       if(idx == arr.length){
          return -1;
       }
-
+      /*
       int liisa = lastIndex(arr, idx + 1, x);
       if(liisa != -1){
          return liisa;
@@ -29,6 +46,19 @@ public class Main {
       } else {
          return -1;
       }
+      */
+      
+      int liisa = lastIndex(arr, idx + 1, x);
+      if(liisa == -1){
+         if(arr[idx] == x){
+            return idx;
+         } else {
+            return -1;
+         }
+      } else {
+         return liisa;
+      }
+      
    }
 
 }

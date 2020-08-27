c1a8ed45a2351e61b594623f0563aed29a80f4d7
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-08-27 21:25:51 +0530

    Update Balanced Brackets.java

--- 06. Basic Data Structures/01. Stack and Queues/02. Balanced Brackets/Balanced Brackets.java
+++ 06. Basic Data Structures/01. Stack and Queues/02. Balanced Brackets/Balanced Brackets.java
@@ -5,14 +5,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        String exp = scn.nextLine();
-        System.out.println( isBalB( exp ) );
+        String str = scn.nextLine();
+        System.out.println( isBalBracket( str ) );
     }
-    public static boolean isBalB(String exp)
+    public static boolean isBalBracket(String str)
     {
         Stack<Character> st = new Stack<>();
-        for (int i = 0; i < exp.length(); i++) {
-            char ch = exp.charAt(i);
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
             if (ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
             } else if (ch == ')') {

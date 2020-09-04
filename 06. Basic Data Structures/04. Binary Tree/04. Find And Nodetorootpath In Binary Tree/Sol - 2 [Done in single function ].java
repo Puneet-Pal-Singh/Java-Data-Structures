b481a6f82aef6b2fa22739c418d5ed32b53f996f
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-09-04 13:17:14 +0530

    Update and rename Sol - 2 [Done in single function ] to Sol - 2 [Done in single function ].java

--- 06. Basic Data Structures/04. Binary Tree/04. Find And Nodetorootpath In Binary Tree/Sol - 2 [Done in single function ].java
+++ 06. Basic Data Structures/04. Binary Tree/04. Find And Nodetorootpath In Binary Tree/Sol - 2 [Done in single function ].java
@@ -139,3 +139,17 @@ public class Main {
     }
 
 }
+
+
+/*
+        Sample Input
+        
+        19
+        50 25 12 n n 37 30 n n n 75 62 n 70 n n 87 n n
+        30
+        
+        Sample Output
+        
+        true
+        [30, 37, 25, 50]
+*/

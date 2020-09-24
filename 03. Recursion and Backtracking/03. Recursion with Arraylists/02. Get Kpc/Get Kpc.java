50c29f77c2d9d67383fd24728d8a3be1e0473c3d
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-09-24 17:46:54 +0530

    Update Get Kpc.java

--- 03. Recursion and Backtracking/03. Recursion with Arraylists/02. Get Kpc/Get Kpc.java
+++ 03. Recursion and Backtracking/03. Recursion with Arraylists/02. Get Kpc/Get Kpc.java
@@ -1,3 +1,6 @@
+
+//  Both answers are good, just play with code
+
 import java.io.*;
 import java.util.*;
 
@@ -6,25 +9,37 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         String str = scn.nextLine();
-        
-        ArrayList<String> ans = getKPC(str);
+
+        ArrayList < String > ans = getKPC(str);
         System.out.println(ans);
     }
-    
-    static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
-        
-    public static ArrayList<String> getKPC(String ques) {
-        
-        if( ques.length() == 0 ){
-            ArrayList<String> baseAns = new ArrayList<>();
+
+    static String[] codes = {
+        ".;",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tu",
+        "vwx",
+        "yz"
+    };
+
+    public static ArrayList < String > getKPC(String str) {
+
+        if (str.length() == 0) {
+            ArrayList < String > baseAns = new ArrayList < > ();
             baseAns.add("");
             return baseAns;
         }
-        
-        ArrayList<String> recAns = getKPC( ques.substring(1) );
+
+        /*
+        ArrayList<String> recAns = getKPC( str.substring(1) );
         ArrayList<String> myAns = new ArrayList<>();
         
-        int idx = ques.charAt(0) - '0';
+        int idx = str.charAt(0) - '0';
         String word = codes[idx];
         
         for( int i = 0; i < word.length(); i++){
@@ -33,10 +48,48 @@ public class Main {
                 myAns.add(ch + recAns.get(j) );
             }
         }
+        
+        */
+
+        // char ch = str.charAt(0);
+        String restStr = str.substring(1);
+        ArrayList < String > recAns = getKPC(restStr);
+        ArrayList < String > myAns = new ArrayList < > ();
+
+        int idx = str.charAt(0) - '0';
+        String word = codes[idx];
+
+        // Everthing in one line 
+        // String word = codes[str.charAt(0) - '0'];
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            // for( int j = 0; j< recAns.size(); j++ ){
+            //     myAns.add(ch + recAns.get(j) );
+            // }
+            for (String rstr: recAns) {
+                myAns.add(ch + rstr);
+            }
+        }
+
+
         return myAns;
     }
 }
 
+
+
+// Codes
+// {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+/*
+        Input---
+        78
+        
+        Output--
+        [tv, tw, tx, uv, uw, ux]
+
+*/
 
 /*
         Input---

f015bdfb72692ed8844e8cf08ebd871e21ecf6a6
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-09-25 20:54:25 +0530

    Update Get Maze Path With Jumps.java

--- 03. Recursion and Backtracking/03. Recursion with Arraylists/05. Get Maze Path With Jumps/Get Maze Path With Jumps.java
+++ 03. Recursion and Backtracking/03. Recursion with Arraylists/05. Get Maze Path With Jumps/Get Maze Path With Jumps.java
@@ -7,39 +7,49 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        ArrayList<String> paths = getMazePaths(0, 0, n - 1, m - 1);
+        // ArrayList < String > paths = getMazePaths(0, 0, n - 1, m - 1);
+        
+        ArrayList < String > paths = getMazePaths(1, 1, n, m );
         System.out.println(paths);
     }
 
-    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
-        if(sr > dr || sc > dc){
-            return new ArrayList<>();
-        }
 
-        if(sr == dr && sc == dc){
-            ArrayList<String> bres = new ArrayList<>();
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static ArrayList < String > getMazePaths(int sr, int sc, int dr, int dc) {
+        // if (sr > dr || sc > dc) {
+        //     return new ArrayList < > ();
+        // }
+
+        if (sr == dr && sc == dc) {
+            ArrayList < String > bres = new ArrayList < > ();
             bres.add("");
             return bres;
         }
 
-        ArrayList<String> paths = new ArrayList<>();
-        for(int move = 1; move <= dc - sc; move++){
-            ArrayList<String> hpaths = getMazePaths(sr, sc + move, dr, dc);
-            for(String hpath: hpaths){
+        // horizontal moves 
+        ArrayList < String > paths = new ArrayList < > ();
+        for (int move = 1; move <= dc - sc; move++) {
+            ArrayList < String > hpaths = getMazePaths(sr, sc + move, dr, dc);      // Faith
+            for (String hpath: hpaths) {
                 paths.add("h" + move + hpath);
             }
         }
-        
-        for(int move = 1; move <= dr - sr; move++){
-            ArrayList<String> vpaths = getMazePaths(sr + move, sc, dr, dc);
-            for(String vpath: vpaths){
+
+        // vertical moves
+        for (int move = 1; move <= dr - sr; move++) {
+            ArrayList < String > vpaths = getMazePaths(sr + move, sc, dr, dc);
+            for (String vpath: vpaths) {
                 paths.add("v" + move + vpath);
             }
         }
 
-        for(int move = 1; move <= dc - sc && move <= dr - sr; move++){
-            ArrayList<String> dpaths = getMazePaths(sr + move, sc + move, dr, dc);
-            for(String dpath: dpaths){
+        // diagonal moves
+        for (int move = 1; move <= dc - sc && move <= dr - sr; move++) {
+            ArrayList < String > dpaths = getMazePaths(sr + move, sc + move, dr, dc);
+            for (String dpath: dpaths) {
                 paths.add("d" + move + dpath);
             }
         }

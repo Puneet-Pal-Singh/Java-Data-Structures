75535b5c5e262447da3185de9acfca27003e34de
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-09-28 20:58:55 +0530

    Update Print Maze Paths With Jumps.java

--- 03. Recursion and Backtracking/04. Recursion on the way up/05. Print Maze Paths With Jumps/Print Maze Paths With Jumps.java
+++ 03. Recursion and Backtracking/04. Recursion on the way up/05. Print Maze Paths With Jumps/Print Maze Paths With Jumps.java
@@ -7,13 +7,15 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
-        printMazePaths( 0, 0, n - 1, m - 1, "");
+        // printMazePaths( 0, 0, n - 1, m - 1, "");
+        printMazePaths( 1, 1, n, m, "");
     }
 
     // sr - source row
     // sc - source column
     // dr - destination row
     // dc - destination column
+    // jump - move size
     public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
         if(sr == dr && sc == dc){
             System.out.println(psf);

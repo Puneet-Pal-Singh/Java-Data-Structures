2b2f1d44b2143ef94d9f086e8c5192c03a3e05be
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-10-01 20:31:00 +0530

    Update N Queens.java

--- 03. Recursion and Backtracking/05. Recursion Backtracking/03. N Queens/N Queens.java
+++ 03. Recursion and Backtracking/05. Recursion Backtracking/03. N Queens/N Queens.java
@@ -1,51 +1,52 @@
 import java.io.*;
 import java.util.*;
-public class Main 
-{
-    public static void main(String[] args) throws Exception 
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int [][] chess=new int[n][n];
-        printNQueens(chess,"",0);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] chess = new int[n][n];
+        
+        printNQueens(chess, "", 0);
         System.out.println();
     }
-    public static boolean isQueenSafe(int[][] chess,int row,int col)
-    {
+    public static boolean isQueenSafe(int[][] chess, int row, int col) {
+        
         //upper left diagonal
-        for(int i=row-1,j=col-1 ; i>=0&&j>=0 ; i--,j--)
-        {
-            if(chess[i][j]==1)
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 1){
                 return false;
+            }
         }
+        
         //same upper col
-        for(int i=row-1;i>=0;i--)
-        {
-            if(chess[i][col]==1)
+        for (int i = row - 1, j = col; i >= 0; i--) {
+            if (chess[i][j] == 1){
                 return false;
+            }
         }
+        
         //upper right diagonal
-        for(int i=row-1,j=col+1 ; i>=0&&j<chess[0].length ; i--,j++)
-        {
-            if(chess[i][j]==1)
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess[0].length; i--, j++) {
+            if (chess[i][j] == 1){
                 return false;
+            }
         }
         return true;
     }
-    public static void printNQueens(int[][] chess, String qsf, int row) 
-    {
-        if(row==chess.length)
-        {
-            System.out.println(qsf+".");
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+        
+        if (row == chess.length) {
+            System.out.println(qsf + ".");
             return;
         }
-        for(int col=0;col<chess[0].length;col++)
-        {
-            if(isQueenSafe(chess,row,col))
-            {
-                chess[row][col]=1;
-                printNQueens(chess,qsf+row+"-"+col+", ",row+1);
-                chess[row][col]=0;
+        
+        for (int col = 0; col < chess[0].length; col++) {
+            
+            // if (isQueenSafe(chess, row, col)) {
+            if (isQueenSafe(chess, row, col) == true) {    
+                chess[row][col] = 1;
+                printNQueens(chess, qsf + row + "-" + col + ", ", row + 1);
+                chess[row][col] = 0;
             }
         }
     }

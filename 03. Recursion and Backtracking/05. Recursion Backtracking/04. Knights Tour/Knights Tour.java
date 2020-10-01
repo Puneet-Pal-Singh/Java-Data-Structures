095a638000761ab243375cef301078abcce04ede
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-10-01 21:56:53 +0530

    Update Knights Tour.java

--- 03. Recursion and Backtracking/05. Recursion Backtracking/04. Knights Tour/Knights Tour.java
+++ 03. Recursion and Backtracking/05. Recursion Backtracking/04. Knights Tour/Knights Tour.java
@@ -1,59 +1,91 @@
 import java.io.*;
 import java.util.*;
-public class Main 
-{
-    public static void main(String[] args) throws Exception 
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int r=sc.nextInt();
-        int c=sc.nextInt();
-        int[][] chess=new int[n][n];
-        printKnightsTour(chess,r,c,1);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int r = scn.nextInt();
+        int c = scn.nextInt();
+        int[][] chess = new int[n][n];
+        printKnightsTour(chess, r, c, 1);
         System.out.println();
     }
-    public static void printKnightsTour(int[][] chess, int r, int c,int nextMove)
-    {
-        if(nextMove == chess.length*chess[0].length)
-        {
-            chess[r][c]=nextMove;
-            displayBoard(chess);
-            chess[r][c]=0;
+
+    /*
+        // Smart calls and normal base case 
+        public static void printKnightsTour(int[][] chess, int r, int c, int nextMove) {
+        
+            if (nextMove == chess.length * chess[0].length) {
+                chess[r][c] = nextMove;
+                displayBoard(chess);
+                chess[r][c] = 0;
+                return;
+            }
+            chess[r][c] = nextMove;
+            if (r - 2 >= 0 && c + 1 < chess[0].length && chess[r - 2][c + 1] == 0) {
+                printKnightsTour(chess, r - 2, c + 1, nextMove + 1);
+            }
+            if (r - 1 >= 0 && c + 2 < chess[0].length && chess[r - 1][c + 2] == 0) {
+                printKnightsTour(chess, r - 1, c + 2, nextMove + 1);
+            }
+            if (r + 1 < chess.length && c + 2 < chess[0].length && chess[r + 1][c + 2] == 0) {
+                printKnightsTour(chess, r + 1, c + 2, nextMove + 1);
+            }
+            if (r + 2 < chess.length && c + 1 < chess[0].length && chess[r + 2][c + 1] == 0) {
+                printKnightsTour(chess, r + 2, c + 1, nextMove + 1);
+            }
+            if (r + 2 < chess.length && c - 1 >= 0 && chess[r + 2][c - 1] == 0) {
+                printKnightsTour(chess, r + 2, c - 1, nextMove + 1);
+            }
+            if (r + 1 < chess.length && c - 2 >= 0 && chess[r + 1][c - 2] == 0) {
+                printKnightsTour(chess, r + 1, c - 2, nextMove + 1);
+            }
+            if (r - 1 >= 0 && c - 2 >= 0 && chess[r - 1][c - 2] == 0) {
+                printKnightsTour(chess, r - 1, c - 2, nextMove + 1);
+            }
+            if (r - 2 >= 0 && c - 1 >= 0 && chess[r - 2][c - 1] == 0) {
+                printKnightsTour(chess, r - 2, c - 1, nextMove + 1);
+            }
+            chess[r][c] = 0;
+        }
+    */
+
+
+    // Smart base case and normal calls  
+    public static void printKnightsTour(int[][] chess, int r, int c, int nextMove) {
+
+        // if knight cross length in row and column
+        // chess[r][c] > 0 then undo
+        if (r < 0 || c < 0 || r >= chess.length || c >= chess[0].length ||
+            chess[r][c] > 0) {
             return;
         }
-        chess[r][c]=nextMove;
-        if(r-2>=0 && c+1<chess[0].length && chess[r-2][c+1]==0){
-            printKnightsTour(chess, r - 2, c + 1 , nextMove + 1);
+        // else if (nextMove == chess.length * chess[0].length) {
+        else if (nextMove == chess.length * chess.length) {
+
+            chess[r][c] = nextMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
         }
-        if(r-1>=0 && c+2<chess[0].length && chess[r-1][c+2]==0){
-            printKnightsTour(chess, r - 1, c + 2 , nextMove + 1);
-        }
-        if(r+1<chess.length && c+2<chess[0].length && chess[r+1][c+2]==0){
-            printKnightsTour(chess, r + 1, c + 2 , nextMove + 1);
-        }
-        if(r+2<chess.length && c+1<chess[0].length && chess[r+2][c+1]==0){
-            printKnightsTour(chess, r + 2, c + 1 , nextMove + 1);
-        }
-        if(r+2<chess.length && c-1>=0 && chess[r+2][c-1]==0){
-            printKnightsTour(chess, r + 2, c - 1 , nextMove + 1);
-        }
-        if(r+1<chess.length && c-2>=0 && chess[r+1][c-2]==0){
-            printKnightsTour(chess, r + 1, c - 2 , nextMove + 1);
-        }
-        if(r-1>=0 && c-2>=0 && chess[r-1][c-2]==0){
-            printKnightsTour(chess, r - 1, c - 2 , nextMove + 1);
-        }
-        if(r-2>=0 && c-1>=0 && chess[r-2][c-1]==0){
-            printKnightsTour(chess, r - 2, c - 1 , nextMove + 1);
-        }
-        chess[r][c]=0;
+        chess[r][c] = nextMove;
+
+        printKnightsTour(chess, r - 2, c + 1, nextMove + 1);
+        printKnightsTour(chess, r - 1, c + 2, nextMove + 1);
+        printKnightsTour(chess, r + 1, c + 2, nextMove + 1);
+        printKnightsTour(chess, r + 2, c + 1, nextMove + 1);
+        printKnightsTour(chess, r + 2, c - 1, nextMove + 1);
+        printKnightsTour(chess, r + 1, c - 2, nextMove + 1);
+        printKnightsTour(chess, r - 1, c - 2, nextMove + 1);
+        printKnightsTour(chess, r - 2, c - 1, nextMove + 1);
+
+        chess[r][c] = 0;
     }
-    public static void displayBoard(int[][] chess)
-    {
-        for(int i = 0; i < chess.length; i++)
-        {
-            for(int j = 0; j < chess[0].length; j++)
-            {
+
+
+    public static void displayBoard(int[][] chess) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
                 System.out.print(chess[i][j] + " ");
             }
             System.out.println();

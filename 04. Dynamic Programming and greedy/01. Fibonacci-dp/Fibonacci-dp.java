9575573884ee641c305b65873c322a09ded6527b
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-10-06 14:58:19 +0530

    Update Fibonacci-dp.java

--- 04. Dynamic Programming and greedy/01. Fibonacci-dp/Fibonacci-dp.java
+++ 04. Dynamic Programming and greedy/01. Fibonacci-dp/Fibonacci-dp.java
@@ -1,66 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) throws Exception {
         // write your code here
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        /*
+
         // Recursive
-        
-        int ans = fib(n);
-        System.out.println(ans);
-        
+        // int ans = fib(n);
+        // System.out.println(ans);
+
         // DP with Memoisation
-        
-        int[] dp = new int[n+1];
-        int ans1 = fibDP(n,dp);
+        int[] dp = new int[n + 1];
+        int ans1 = fibDP(n, dp);
         System.out.println(ans1);
-        */
-        
+
         //DP with Tabulation
-        int ans2 = fibT(n);
-        System.out.println(ans2);
+        // int ans2 = fibT(n);
+        // System.out.println(ans2);
     }
-    
+
     // DP with tabulation
-    public static int fibT(int n){
-        if(n==1 || n==0){
+    public static int fibT(int n) {
+        if (n == 1 || n == 0) {
             return n;
         }
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
-        
-        for( int i = 2; i <= n; i++ ){
-            dp[i] = dp[i-1] + dp[i-2];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
-    
+
     //DP with Memoisation
-    public static int fibDP(int n , int[] dp){
-        if(n==1 || n==0){
+    public static int fibDP(int n, int[] dp) {
+        if (n == 1 || n == 0) {
             return n;
-        }else if(dp[n] != 0){
+        } else if (dp[n] != 0) {
             return dp[n];
         }
-        int nm1 = fibDP(n-1 , dp);
-        int nm2 = fibDP(n-2 , dp);
+        int nm1 = fibDP(n - 1, dp);
+        int nm2 = fibDP(n - 2, dp);
         int myans = nm1 + nm2;
         dp[n] = myans;
         return myans;
     }
-    
+
     // Recursion
-    public static int fib(int n){
-        if(n==1 || n==0){
+    public static int fib(int n) {
+        if (n == 1 || n == 0) {
             return n;
         }
-        int nm1 = fib(n-1);
-        int nm2 = fib(n-2);
+        int nm1 = fib(n - 1);
+        int nm2 = fib(n - 2);
         int myAns = nm1 + nm2;
         return myAns;
     }

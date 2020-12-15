dab016d4d7932186f9e7176d8c728f666840830d
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-12-15 17:13:41 +0530

    Update Min Cost In Maze Traversal.java

--- 04. Dynamic Programming and greedy/05. Min Cost in Maze Traversal/Min Cost In Maze Traversal.java
+++ 04. Dynamic Programming and greedy/05. Min Cost in Maze Traversal/Min Cost In Maze Traversal.java
@@ -8,45 +8,48 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
-        int[][] cost = new int[n][m];
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = 0 ; j < m ; j++ ){
-                cost[i][j] = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
             }
         }
-        int ans = minCostMaze( cost );
-        System.out.println( ans );
+        int ans = minCostMaze(arr);
+        System.out.println(ans);
     }
-    
-    public static int minCostMaze( int[][] cost ){
-        int[][] dp = new int[cost.length][cost[0].length];
-        
-        for( int i = dp.length - 1; i >=0 ; i-- ){
-            for( int j = dp[0].length - 1; j >= 0 ; j-- ){
-                
+
+    public static int minCostMaze(int[][] arr) {
+        int[][] dp = new int[arr.length][arr[0].length];
+
+        for (int i = dp.length - 1; i >= 0; i--) {
+            for (int j = dp[0].length - 1; j >= 0; j--) {
+
                 // last element of row and col
-                if( i == dp.length - 1 && j == dp[0].length - 1){
-                    dp[i][j] = cost[i][j];
+                if (i == dp.length - 1 && j == dp[0].length - 1) {
+                    dp[i][j] = arr[i][j];
                 }
-                
+
                 // last row
-                else if( i == dp.length - 1 ){
-                    dp[i][j] = cost[i][j] + dp[i][j+1];
+                else if (i == dp.length - 1) {
+                    dp[i][j] = dp[i][j + 1] + arr[i][j];
                 }
-                
+
                 //last column
-                else if( j == dp[0].length - 1){
-                    dp[i][j] = cost[i][j] + dp[i+1][j];
+                else if (j == dp[0].length - 1) {
+                    dp[i][j] = dp[i + 1][j] + arr[i][j];
                 }
-                
+
                 // Rest
-                else{
-                    int minCost = dp[i][j+1];
-                    if( minCost > dp[i+1][j]){
-                        minCost = dp[i+1][j];
-                    // int minCost = Math.min( dp[i][j+1], dp[i+1][j] );    
+                else {
+                    int minCost = dp[i][j + 1];
+                    if (minCost > dp[i + 1][j]) {
+                        minCost = dp[i + 1][j];
+                        // int minCost = Math.min( dp[i][j+1], dp[i+1][j] );    
                     }
-                    dp[i][j] = cost[i][j] + minCost;
+                    dp[i][j] = minCost + arr[i][j];
+
+                    // one liner
+                    // dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + arr[i][j];
                 }
             }
         }

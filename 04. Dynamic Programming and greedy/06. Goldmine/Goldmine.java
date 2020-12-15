ae4306ce6de4bac31762ffe4cd2b79ad1fcb10dc
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-12-15 18:16:24 +0530

    Update Goldmine.java

--- 04. Dynamic Programming and greedy/06. Goldmine/Goldmine.java
+++ 04. Dynamic Programming and greedy/06. Goldmine/Goldmine.java
@@ -8,52 +8,57 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
-        
+
         int[][] gold = new int[n][m];
-        
-        for( int i = 0 ; i < n ; i++ ){
-            for( int j = 0 ; j < m ; j++ ){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 gold[i][j] = scn.nextInt();
             }
         }
-        int ans = maxGold( gold );
-        System.out.println( ans );
+        int ans = maxGold(gold);
+        System.out.println(ans);
     }
-    
-    public static int maxGold( int[][] gold ){
-        
-        int[][] dp = new int[ gold.length ][ gold[0].length ];
-        
-        for( int j = dp[0].length - 1; j >= 0 ; j-- ){
-            for( int i = 0; i < dp.length ; i++ ){
-                
+
+    public static int maxGold(int[][] gold) {
+
+        int[][] dp = new int[gold.length][gold[0].length];
+
+        // loop of j
+        // as traversing from right col to left col
+        for (int j = dp[0].length - 1; j >= 0; j--) {
+            for (int i = 0; i < dp.length; i++) {
+
                 // last Column
-                if( j == dp[0].length - 1 ){
+                if (j == dp[0].length - 1) {
                     dp[i][j] = gold[i][j];
                 }
-                
+
                 // First Row
-                else if( i == 0 ){
-                    int max = Math.max( dp[ i ][ j+1 ], dp[ i+1 ][ j+1 ] );   
+                else if (i == 0) {
+                    int max = Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
                     dp[i][j] = gold[i][j] + max;
                 }
-                
+
                 // Last Row
-                else if( i == dp.length - 1 ){
-                    int max = Math.max( dp[ i ][ j+1 ], dp[ i-1 ][ j+1 ] );   
+                else if (i == dp.length - 1) {
+                    int max = Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
                     dp[i][j] = gold[i][j] + max;
                 }
-                
+
                 // Rest
-                else{
-                    int max = Math.max( dp[ i-1 ][ j+1 ], Math.max( dp[ i ][ j+1 ], dp[ i+1 ][ j+1 ] ) );   
+                else {
+                    int max = Math.max(dp[i - 1][j + 1], Math.max(dp[i][j + 1], dp[i + 1][j + 1]));
                     dp[i][j] = gold[i][j] + max;
                 }
             }
         }
+
+        // First column
         int ans = dp[0][0];
-        for( int i = 1 ; i < dp.length ; i++ ){
-            if( ans < dp [i][0] ){
+        for (int i = 1; i < dp.length; i++) {
+            // moving in row and checking max
+            if (dp[i][0] > ans) {
                 ans = dp[i][0];
             }
         }

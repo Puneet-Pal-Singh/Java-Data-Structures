d34dd0511dae44b8adf2178532e0b8fcfa13487c
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2020-12-21 21:50:57 +0530

    Update Zero One Knapsack.java

--- 04. Dynamic Programming and greedy/10. Zero One Knapsack/Zero One Knapsack.java
+++ 04. Dynamic Programming and greedy/10. Zero One Knapsack/Zero One Knapsack.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+    /*
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
@@ -21,13 +22,30 @@ public class Main {
 
         int cap = Integer.parseInt(br.readLine());
 
+    */
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] vals = new int[n];
+        int[] wts = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            vals[i] = scn.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            wts[i] = scn.nextInt();
+        }
+
+        int cap = scn.nextInt();
+
         int[][] dp = new int[n + 1][cap + 1];
         for (int i = 1; i < dp.length; i++) {
-            for(int j = 1; j < dp[0].length; j++){
-                int val = values[i - 1];
+            for (int j = 1; j < dp[0].length; j++) {
+                int val = vals[i - 1];
                 int wt = wts[i - 1];
 
-                if(j >= wt){
+                if (j >= wt) {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - wt] + val);
                 } else {
                     dp[i][j] = dp[i - 1][j];
